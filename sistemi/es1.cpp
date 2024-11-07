2cf823edfb83d4f41553e5e964eabdbedf03e75f
Tuga <61603718+Tugamer89@users.noreply.github.com>
2024-11-07 17:11:55 +0100

    Refactored code in es1.cpp and es2.cpp: reformatted code, renamed variables, added comments, and reorganized functions.

--- sistemi/es1.cpp
+++ sistemi/es1.cpp
@@ -4,44 +4,54 @@
 
 using namespace std;
 
-double InfiteNorm(const vector<vector<double>>& matrix)
-{
-    double max = 0.0;
-    for(int i = 0; i < matrix.size(); ++i)
-    {
+double infiteNorm(const vector<vector<double>>& matrix) {
+    double maxRow = 0.0;
+
+    for(size_t i = 0; i < matrix.size(); ++i) {
         double sum = 0.0;
-        for(int j = 0; j < matrix[i].size(); ++j)
-            sum+=abs(matrix[i][j]);
-        if(sum > max)
-            max = sum;
+        for(size_t j = 0; j < matrix[i].size(); ++j)
+            sum += abs(matrix[i][j]);
+        maxRow = max(sum, maxRow);
     }
-    return max;
+
+    return maxRow;
 }
 
 int main() {
-    int d1 = 1, d0 = 6; //ultime cifre matricola Tuillier S5441316
+    // Matricola Tuillier 5441316
+    int d1 = 1, d0 = 6;
     int n = 10*(d1 + 1) + d0;
-    vector<vector<double>> A1 = {{3, 1, -1, 0}, {0, 7, -3, 0}, {0, -3, 9, -2}, {0, 0, 4, -10}};
-    vector<vector<double>> A2 = {{2, 4, -2, 0}, {1, 3, 0, 1}, {3, -1, 1, 2}, {0, -1, 2, 1}};
+
+    vector<vector<double>> A1 = {
+        {3, 1, -1, 0},
+        {0, 7, -3, 0},
+        {0, -3, 9, -2},
+        {0, 0, 4, -10}
+    };
+
+    vector<vector<double>> A2 = {
+        {2, 4, -2, 0},
+        {1, 3, 0, 1},
+        {3, -1, 1, 2},
+        {0, -1, 2, 1}
+    };
+    
     vector<vector<double>> P, T;
 
-    //popolare P
-    for(int i = 0; i < 10; ++i)
-    {
+    // Popolare P
+    for(size_t i = 0; i < 10; ++i) {
         P.push_back(vector<double>(10));
-        for(int j = 0; j < 10; ++j)
-            P[i][j] = tgamma(i + j - 1) / (tgamma(i) * tgamma(j)); //tgamma(n) = (n-1)!
+        for(size_t j = 0; j < 10; ++j)
+            P[i][j] = tgamma(i + j + 1) / (tgamma(i + 1) * tgamma(j + 1)); // tgamma(n) = (n-1)!
     }
 
-    //popolare T
-    for (int i = 0; i < n; ++i)
-    {
+    // Popolare T
+    for (ssize_t i = 0; i < n; ++i) {
         T.push_back(vector<double>(n));
-        for (int j = 0; j < n; ++j)
-        {
-            if(i == j)
+        for (ssize_t j = 0; j < n; ++j) {
+            if (i == j)
                 T[i][j] = 2;
-            else if(abs(i - j) == 1)
+            else if (abs(i - j) == 1)
                 T[i][j] = -1;
             else
                 T[i][j] = 0;
@@ -49,11 +59,10 @@ int main() {
     }
     
 
-    
-    cout << "Infite Norm of A1: " << InfiteNorm(A1) << endl;
-    cout << "Infite Norm of A2: " << InfiteNorm(A2) << endl;
-    cout << "Infite Norm of P: " << InfiteNorm(P) << endl;
-    cout << "Infite Norm of T: " << InfiteNorm(T) << endl;
+    cout << "Infite Norm of A1: " << infiteNorm(A1) << endl;
+    cout << "Infite Norm of A2: " << infiteNorm(A2) << endl;
+    cout << "Infite Norm of P:  " << infiteNorm(P) << endl;
+    cout << "Infite Norm of T:  " << infiteNorm(T) << endl;
     
     return 0;
 }
--- sistemi/es2.cpp
+++ sistemi/es2.cpp
@@ -4,85 +4,36 @@
 
 using namespace std;
 
+void print_matrix(float **matrix, size_t n, size_t m) {
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < m; ++j)
+            printf("%f ", matrix[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
 
-    void swap_rows(float **matrix, size_t a, size_t b) {
+float** compute_b(float **A, size_t n) {
+    // Moltiplicare A per un vettore di soli 1 e` uguale a sommare ogni elemento della stessa riga
+    float **b = new float*[n];
+    for (size_t i = 0; i < n; ++i) {
+        b[i] = new float[1];
+        b[i][0] = 0;
+
+        for (size_t j = 0; j < n; ++j)
+            b[i][0] += A[i][j];
+    }
+
+    return b;
+}
+
+void swap_rows(float **matrix, size_t a, size_t b) {
     float *tmp = matrix[a];
     matrix[a] = matrix[b];
     matrix[b] = tmp;
 }
 
-int main() {
-    int d1 = 1, d0 = 6; //ultime cifre matricola Tuillier S5441316
-    int n = 10*(d1 + 1) + d0;
-    float** A1 = new float*[4];
-    float** A2 = new float*[4];
-
-    //popolare A1
-    float A1arr[4][4] = {
-        {3, 1, -1, 0}, {0, 7, -3, 0}, {0, -3, 9, -2}, {0, 0, 4, -10}
-    };
-
-    for (int i = 0; i < 4; ++i) {
-        A1[i] = new float[4];
-        for (int j = 0; j < 4; ++j)
-            A1[i][j] = A1arr[i][j];
-    }
-
-    //popolare A2
-    float A2arr[4][4] = {
-        {2, 4, -2, 0}, {1, 3, 0, 1}, {3, -1, 1, 2}, {0, -1, 2, 1}
-    };
-
-    for (int i = 0; i < 4; ++i) {
-        A2[i] = new float[4];
-        for (int j = 0; j < 4; ++j)
-            A2[i][j] = A2arr[i][j];
-    }
-
-    float** P = new float*[10];
-    float** T = new float*[n];
-
-    //popolare P
-    for(int i = 0; i < 10; ++i)
-    {
-        P[i] = new float[10];
-        for(int j = 0; j < 10; ++j)
-            P[i][j] = tgamma(i + j - 1) / (tgamma(i) * tgamma(j)); //tgamma(n) = (n-1)!
-    }
-
-    //popolare T
-    for (int i = 0; i < n; ++i)
-    {
-        T[i] = new float[n];
-        for (int j = 0; j < n; ++j)
-        {
-            if(i == j)
-                T[i][j] = 2;
-            else if(abs(i - j) == 1)
-                T[i][j] = -1;
-            else
-                T[i][j] = 0;
-        }
-    }
-
-    float **A, **b, **X;
-    size_t n, dim_A, dim_b;
-
-    // Ask for size and matrix
-    printf("Enter size of matrix (n): ");
-    scanf("%zu", &n);
-    create_matrix(&A, n, n);
-    create_matrix(&b, n, 1);
-    create_matrix(&X, n, 1);
-    
-    printf("\nEnter matrix A:\n");
-    popolate_matrix(A, n, n);
-    printf("\nEnter matrix b:\n");
-    popolate_matrix(b, n, 1);
-
-    printf("\nSteps:\n");
-
-    // Gaussian elimination
+bool gaussian_elimination(float**& A, float**& b, size_t n) {
     for (size_t k = 0; k < n-1; ++k) {
         size_t max_pivot = k;
 
@@ -91,7 +42,7 @@ int main() {
                 max_pivot = i;
 
         if (max_pivot != k) {
-            printf("Swapping R%zu with R%zu\n", k+1, max_pivot+1);
+            fprintf(stderr, "Swapping R%zu with R%zu\n", k+1, max_pivot+1);
             swap_rows(A, k, max_pivot);
             swap_rows(b, k, max_pivot);
         }
@@ -100,7 +51,7 @@ int main() {
         
         if (pivot == 0) {
             printf("Matrix is singular\n");
-            return 1;
+            return false;
         }
 
         for (size_t i = k+1; i < n; ++i) {
@@ -114,26 +65,129 @@ int main() {
         }
     }
 
-    printf("\n");
+    return true;
+}
 
-    // Back subsitution
+float** back_substitution(float **A, float **b, size_t n) {
+    float **X = new float*[n];
+    for (size_t i = 0; i < n; ++i) {
+        X[i] = new float[1];
+        X[i][0] = 0;
+    }
+
     X[n-1][0] = b[n-1][0] / A[n-1][n-1];
-    for (long long i = n-2; i >= 0; --i) {
+    for (ssize_t i = n-2; i >= 0; --i) {
         float sum = 0;
         for (size_t j = i+1; j < n; ++j)
             sum += A[i][j] * X[j][0];
         X[i][0] = (b[i][0] - sum) / A[i][i];
     }
 
-    // Print A & b reduced
-    printf("A:\n");
-    print_matrix(A, n, n);
+    return X;
+}
 
-    printf("b:\n");
-    print_matrix(b, n, 1);
+int main() {
+    // Matricola Tuillier 5441316
+    int d1 = 1, d0 = 6;
+    int n = 10*(d1 + 1) + d0;
 
-    printf("X:\n");
-    print_matrix(X, n, 1);
+    float** A1 = new float*[4];
+    float** A2 = new float*[4];
+    float** P = new float*[10];
+    float** T = new float*[n];
+
+    // Popolare A1
+    float initA1[4][4] = {
+        {3, 1, -1, 0},
+        {0, 7, -3, 0},
+        {0, -3, 9, -2},
+        {0, 0, 4, -10}
+    };
+
+    for (size_t i = 0; i < 4; ++i) {
+        A1[i] = new float[4];
+        for (size_t j = 0; j < 4; ++j)
+            A1[i][j] = initA1[i][j];
+    }
+
+    // Popolare A2
+    float initA2[4][4] = {
+        {2, 4, -2, 0},
+        {1, 3, 0, 1},
+        {3, -1, 1, 2},
+        {0, -1, 2, 1}
+    };
+
+    for (size_t i = 0; i < 4; ++i) {
+        A2[i] = new float[4];
+        for (size_t j = 0; j < 4; ++j)
+            A2[i][j] = initA2[i][j];
+    }
+
+    // Popolare P
+    for(size_t i = 0; i < 10; ++i) {
+        P[i] = new float[10];
+        for(size_t j = 0; j < 10; ++j)
+            P[i][j] = tgamma(i + j + 1) / (tgamma(i + 1) * tgamma(j + 1)); //tgamma(n) = (n-1)!
+    }
+
+    // Popolare T
+    for (ssize_t i = 0; i < n; ++i) {
+        T[i] = new float[n];
+        for (ssize_t j = 0; j < n; ++j) {
+            if (i == j)
+                T[i][j] = 2;
+            else if (abs(i - j) == 1)
+                T[i][j] = -1;
+            else
+                T[i][j] = 0;
+        }
+    }
+
+    // Stampa le A
+    printf("A1:\n");
+    print_matrix(A1, 4, 4);
+    printf("A2:\n");
+    print_matrix(A2, 4, 4);
+    printf("P:\n");
+    print_matrix(P, 10, 10);
+    printf("T:\n");
+    print_matrix(T, n, n);
+    printf("\n\n");
+
+    // Poplare le b
+    float **bA1 = compute_b(A1, 4);
+    float **bA2 = compute_b(A2, 4);
+    float **bP = compute_b(P, 10);
+    float **bT = compute_b(T, n);
+
+    // Stampa le b
+    printf("bA1:\n");
+    print_matrix(bA1, 4, 1);
+    printf("bA2:\n");
+    print_matrix(bA2, 4, 1);
+    printf("bP:\n");
+    print_matrix(bP, 10, 1);
+    printf("bT:\n");
+    print_matrix(bT, n, 1);
+    printf("\n\n");
+
+    // Popolare le X
+    float **XA1 = back_substitution(A1, bA1, 4);
+    float **XA2 = back_substitution(A2, bA2, 4);
+    float **XP = back_substitution(P, bP, 10);
+    float **XT = back_substitution(T, bT, n);
+
+    // Stampa le X
+    printf("XA1:\n");
+    print_matrix(XA1, 4, 1);
+    printf("XA2:\n");
+    print_matrix(XA2, 4, 1);
+    printf("XP:\n");
+    print_matrix(XP, 10, 1);
+    printf("XT:\n");
+    print_matrix(XT, n, 1);
+    printf("\n\n");
 
     return 0;
 }
