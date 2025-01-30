15202365fe9dee053105fe6f0d6b3d439960ddbf
Tuga <61603718+Tugamer89@users.noreply.github.com>
2025-01-30 16:46:21 +0100

    Improved code

--- autovalori/es3.m
+++ autovalori/es3.m
@@ -1,47 +1,59 @@
 % ES 3
 clear
 clc
-disp("ES 3")
+disp("ES 3");
 
 A = [ 1  -1  2;
      -2   0  5;
       6  -3  6];
 
+maxIter = 2000;
+
     % PARTE 1
-function v = powerMethod(A, v0, maxIter)
+function lambda = powerMethod(A, v0, maxIter)
     v = v0 / norm(v0);
-    
-    startColor = [0, 0, 1]; % Blu
-    endColor = [1, 0, 0];   % Rosso
-    
-    % Prepara la finestra grafica
-    figure;
-    hold on;
-    grid on;
-    xlabel('x'); ylabel('y'); zlabel('z');
     
     for k = 1:maxIter
         v_new = A * v;
         v = v_new / norm(v_new);
-        
-        t = (k - 1) / (maxIter - 1); % Valore tra 0 e 1
-        currentColor = (1 - t) * startColor + t * endColor;
-        
-        plot3([0, v(1)], [0, v(2)], [0, v(3)], 'Color', currentColor, 'LineWidth', 1);
+        lambda = v' * A * v;
     end
-    
-    hold off; % Chiudi la modalità di sovrapposizione
 end
 
 v1 = [1; 1; 1];
 v2 = [3; 10; 4];
-maxIter = 100;
 
-V1 = powerMethod(A, v1, maxIter);
-V2 = powerMethod(A, v2, maxIter);
+lambda1 = powerMethod(A, v1, maxIter);
+lambda2 = powerMethod(A, v2, maxIter);
 
-disp("Autovettore di v1 = ");
-disp(V1);
+fprintf("\nAutovalore stimato (v1): %.6E\n", lambda1);
+fprintf("Autovalore stimato (v2): %.6E\n", lambda2);
 
-disp("Autovettore di v2 = ");
-disp(V2);
+    % PARTE 2
+function lambda = inversePowerMethod(A, p, v0, maxIter)
+    B = inv(A - p*eye(size(A)));
+    mi = powerMethod(B, v0, maxIter);
+    lambda = 1/mi + p;
+end
+
+
+eig_vals = eig(A);
+
+lambda_inv1 = inversePowerMethod(A, 100, v1, maxIter);
+lambda_inv2 = inversePowerMethod(A, 100, v2, maxIter);
+
+
+fprintf("\nAutovalore stimato con metodo inverso (v1): %.6E\n", lambda_inv1);
+fprintf("Autovalore stimato con metodo inverso (v2): %.6E\n", lambda_inv2);
+
+fprintf("\nAutovalori reali della matrice:\n");
+fprintf("[%.6E, %.6E, %.6E]\n", eig_vals);
+
+
+fprintf("\nDifferenza metodo potenze (v1): |%.6E - %.6E| = %.6E\n", max(abs(eig_vals)), lambda1, abs(max(abs(eig_vals)) - lambda1));
+fprintf("Differenza metodo potenze (v2): |%.6E - %.6E| = %.6E\n", max(abs(eig_vals)), lambda2, abs(max(abs(eig_vals)) - lambda2));
+fprintf("Differenza metodo potenze inverse (v1): |%.6E - %.6E| = %.6E\n", max(abs(eig_vals)), lambda_inv1, abs(max(abs(eig_vals)) - lambda_inv1));
+fprintf("Differenza metodo potenze inverse (v2): |%.6E - %.6E| = %.6E\n", max(abs(eig_vals)), lambda_inv2, abs(max(abs(eig_vals)) - lambda_inv2));
+
+fprintf("\nVelocità di convergenza metodo delle potenze = %.6E\n", abs(max(lambda1, lambda2)/min(lambda1, lambda2))^maxIter)
+fprintf("Velocità di convergenza metodo delle potenze inverse = %.6E\n", abs(max(lambda_inv1, lambda_inv2)/min(lambda_inv1, lambda_inv2))^maxIter)
